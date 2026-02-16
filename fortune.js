a8129311d9cffc20c1b772ba5ab23f02ddfe8797
MariMode1 <modebadzemari65@gmail.com>
2026-02-15 23:33:32 -0500

    Added song lyrics and mustang feature

--- fortune.js
+++ fortune.js
@@ -4,15 +4,30 @@ import * as cowsay from "cowsay";
 // YOUR FORTUNES: Replace these with your own!
 // Pick a theme that interests you: motivational quotes, coding tips,
 // jokes, song lyrics, advice to your future self — whatever you want.
+
+// Lana Del Rey - White Mustang lyrics:https://genius.com/Lana-del-rey-white-mustang-lyrics
+
 const fortunes = [
-  "Welcome to JavaScript!",
-  "Arrays start at index 0",
-  "Keep calm and code on",
+  "White Mustang.",
+  "You're a wild Mustang.",
+  "The day I saw your White Mustang.",
 ];
 
 // Pick a random fortune
 const randomIndex = Math.floor(Math.random() * fortunes.length);
 const todaysFortune = fortunes[randomIndex];
 
-// Display the cow (or change the creature — it's your project!)
-console.log(cowsay.say({ text: todaysFortune }));
+// Mood Feature for the Mustang
+
+const moods = ["oo", "^^", "--"];
+const randomMood = moods[Math.floor(Math.random() * moods.length)];
+
+// Display a White Mustang 
+
+console.log(
+  cowsay.say({ 
+    text: todaysFortune, 
+    f: "Whote Mustang",
+    e: randomMood,
+  })
+);
